c7d9b15feb80c8695b358611c8b1c6ab59c2410c
Rakesh <rakeshrh6767@gmail.com>
2025-05-08 14:08:44 +0530

    Add files via upload

---
+++ app_gradio.py
@@ -0,0 +1,29 @@
+import gradio as gr
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
+
+# Path to your extracted model directory
+#model_path = "./feedback_model"  # Change this!
+
+# Load model and tokenizer
+model = BertForSequenceClassification.from_pretrained("./src/feedback_model",local_files_only=True)
+tokenizer = BertTokenizer.from_pretrained("./src/feedback_model",local_files_only=True)
+
+# Prediction function
+def predict(text, reason):
+    inputs = tokenizer(text, reason, return_tensors="pt", truncation=True, padding=True)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    pred = torch.argmax(outputs.logits, dim=1).item()
+    return "Matched" if pred == 1 else "Not Matched"
+
+# Gradio UI
+iface = gr.Interface(
+    fn=predict,
+    inputs=["text", "text"],
+    outputs="text",
+    title="Feedback Validator",
+    description="Check if feedback and reason are logically aligned."
+)
+
+iface.launch()
